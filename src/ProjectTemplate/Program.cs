4d679878ec1e9dcc036d3c7cf6ea9c94f0511532
Kelly Adams <kelly@intellitect.com>
2022-06-20 17:10:17 -0700

    Create feeble console app

---
+++ src/ProjectTemplate/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using ICanHazDadJoke.NET;
+
+namespace ProjectTemplate;
+
+
+public class Program
+{
+    private static async Task<int> Main(string[] args)
+    {
+        var client = new DadJokeClient("A feeble dotnet project template example", "https://github.com/IntelliTect/");
+
+        var joke = await client.GetRandomJokeStringAsync();
+
+        Console.WriteLine(joke);
+
+        return await Task.FromResult(0);
+    }
+}
+
+
+
